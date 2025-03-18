4e2f31c2ace202c72b677499cdfefdf523213bf8
zzannorita <tpals0518@naver.com>
2025-03-18 23:13:30 +0900

    Style: 작은 따옴표 큰 따옴표로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import "./index.css"; 
-import '../src/styles/tailwind.css';
-import Header from './components/Header';
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Footer from './components/Footer';
-
-
+import "./index.css";
+import "../src/styles/tailwind.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Footer from "./components/Footer";
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
@@ -19,6 +18,7 @@ function App() {
       <Skills />
       <Projects />
       <Footer />
+      <SideBar />
     </div>
   );
 }
