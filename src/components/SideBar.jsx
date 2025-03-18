a4f06e7b0d1786b04afe7d642cff83cbf48b5cf8
zzannorita <tpals0518@naver.com>
2025-03-18 23:13:46 +0900

    Test: 사이드바 임시 생성

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SideBar() {
+  return (
+    <div className="fixed top-4 right-4">
+      <div>
+        <img src="/src/images/pin.png" alt="pin" className="w-16 h-16" />
+      </div>
+      <div></div>
+    </div>
+  );
+}
