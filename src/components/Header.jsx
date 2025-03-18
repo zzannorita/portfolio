d636aa07b26d94e2f61090049fb1b059a306180b
zzannorita <tpals0518@naver.com>
2025-03-18 21:35:36 +0900

    스타일 및 설정 파일 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,53 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
-    <header className="bg-white border-b border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+    <header className="w-full border-b border-black-100 bg-white">
       {/* 상단바 */}
-      <div className="flex justify-between items-center px-4 py-2">
-        <div className="text-black dark:text-white">다크모드</div>
-        <div className="text-black dark:text-white">노래</div>
+      <div className="flex justify-end max-w-screen-xl mx-auto pt-4 ">
+        <div className="flex justify-between w-24">
+          <img src="/src/images/sun.png" alt="light" className="w-6 h-6" />
+          <div className="font-retro text-gray-800">sound on</div>
+        </div>
       </div>
 
       {/* 메인헤더 */}
-      <div className="p-4">메인 헤더 영역</div>
-      <button>버튼</button>
+      <div className="flex justify-between max-w-screen-xl mx-auto py-2 ">
+        <div className="flex justify-between items-start w-full h-10">
+          <a href="/" aria-label="home" className="flex">
+            <img
+              src="/src/images/silverStar.gif"
+              alt="logo"
+              className="w-8 h-8"
+            />
+            <h1 className="text-2xl font-bold flex font-retro">zzannorita</h1>
+          </a>
+          <nav>
+            <ul className="flex space-x-6 font-retro text-lg">
+              <li>
+                <a href="#Home" className="hover:text-yellow-500">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#About" className="hover:text-yellow-500">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#Skills" className="hover:text-yellow-500">
+                  Skills
+                </a>
+              </li>
+              <li>
+                <a href="#Projects" className="hover:text-yellow-500">
+                  Projects
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </header>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Pretendard", "Arial", "sans-serif"],
+        retro: ["DungGeunMo", "serif"],
+        logo: ["Cafe24Nyangi-W-v1.0", "Arial", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
 };
