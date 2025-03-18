39e396f808a02e3879cbc4d45b297174fcac0065
zzannorita <tpals0518@naver.com>
2025-03-18 23:15:25 +0900

    Test: 모바일에서 테스트 코드 임시 생성

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
   plugins: [react(), eslint()],
+  server: {
+    host: true, // 외부 접속 허용
+    port: 5173, // Vite 기본 포트
+    strictPort: true, // 지정한 포트 유지
+    allowedHosts: ["bf18-110-12-78-98.ngrok-free.app"], // ngrok 도메인 추가
+  },
 });
