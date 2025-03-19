b176b9b238136ec8b39b7a55c295a067f3cd0aa1
zzannorita <tpals0518@naver.com>
2025-03-19 16:24:35 +0900

    Test: 홈페이지 테스트 코드에 마진 적용

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-screen-xl mx-auto">
       로렘 입숨(lorem ipsum; 줄여서 립숨, lipsum)은 출판이나 그래픽 디자인
       분야에서 폰트, 타이포그래피, 레이아웃 같은 그래픽 요소나 시각적 연출을
       보여줄 때 사용하는 표준 채우기 텍스트로, 최종 결과물에 들어가는 실제적인
