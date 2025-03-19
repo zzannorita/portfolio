3122a579620708bd3b7c995b478e7552888e2049
zzannorita <tpals0518@naver.com>
2025-03-19 16:21:39 +0900

    Chore: tailwind jit 모드 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: "jit",
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
@@ -7,6 +8,9 @@ export default {
         sans: ["Pretendard", "Arial", "sans-serif"],
         retro: ["DungGeunMo", "serif"],
         logo: ["Cafe24Nyangi-W-v1.0", "Arial", "sans-serif"],
+      },
+      boxShadow: {
+        custom: "0px 1px 9px rgba(0, 0, 0, 0.2)",
       },
     },
   },
