ba6d8f9a72eee9e3c6d32eba49f9d95f91482b4e
zzannorita <tpals0518@naver.com>
2025-03-20 15:26:12 +0900

    Design: 로고 폰트 텐나다로 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
       fontFamily: {
         sans: ["Pretendard", "Arial", "sans-serif"],
         retro: ["DungGeunMo", "serif"],
-        logo: ["Cafe24Nyangi-W-v1.0", "Arial", "sans-serif"],
+        logo: ["Tenada", "Arial", "sans-serif"],
       },
       boxShadow: {
         custom: "0px 1px 9px rgba(0, 0, 0, 0.2)",
