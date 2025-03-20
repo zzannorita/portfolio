044a85170ab64760ec22580a59b82fb1d07269dd
zzannorita <tpals0518@naver.com>
2025-03-20 16:07:47 +0900

    Design: 홈페이지 ui 수정

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const bgImgs = [
-  "/src/images/bg.png",
-  "/src/images/bg-2.png",
-  "/src/images/bg-3.png",
+  "/src/images/bg.jpg",
+  "/src/images/bg-2.jpg",
+  "/src/images/bg-3.jpg",
 ];
 
 export default function Home() {
   const [currentImg, setCurrentImg] = useState(0);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImg((prev) => (prev + 1) % bgImgs.length);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div>
-      <img src={bgImgs[currentImg]} alt="carousel" className="w-full" />
-      <div className="absolute top-1/3 left-1/3 transform -translate-x-1/2"></div>
-      <div className="absolute top-[325px] right-16 flex flex-col space-y-8 transform -translate-y-1/2 ">
+    <div className="relative w-full h-[40vh] sm:h-[50vh] lg:h-[70vh]">
+      <img
+        src={bgImgs[currentImg]}
+        alt="carousel"
+        className="w-full h-full object-cover transition-opacity duration-700 ease-in-out"
+      />
+      <div className="absolute top-[45%] left-[8%] sm:top-[45%] sm:left-[15%] lg:top-[45%] lg:left-[17%] font-logo text-white text-shadow">
+        <h1 className=" text-[1.5rem] sm:text-[2rem] lg:text-[3.5rem]">
+          web front-end developer portfolio
+        </h1>
+        <h3 className="text-[1rem] sm:text-[1.5rem] lg:text-[2.5rem]">
+          끊임없이 도전하는 개발자 장유리입니다
+        </h3>
+      </div>
+      <div className="hidden sm:flex absolute top-[55%] right-8 flex-col space-y-6 transform -translate-y-1/2 ">
         {bgImgs.map((_, index) => (
           <button
             key={index}
             onClick={() => setCurrentImg(index)}
-            className={`w-5 h-5 rounded-full border border-gray-600 ${
-              currentImg === index ? "bg-gray-400" : "bg-gray-100"
+            className={`w-4 h-4 lg:w-5 lg:h-5 rounded-full border border-gray-600 ${
+              currentImg === index ? "bg-yellow-500" : "bg-gray-100"
             }`}
           ></button>
         ))}
