9c089783f84b4b2ffb96a84e89022ce86088eba3
zzannorita <tpals0518@naver.com>
2025-04-01 19:52:26 +0900

    Fix: 헤더 로고이미지 변경 및 폰트 기본체로 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,24 +7,24 @@ export default function Header() {
       {/* 상단바 */}
       <div className="sm:hidden flex justify-between items-center max-w-screen-xl mx-auto pt-2">
         <div className="flex items-center">
-          <a href="/" aria-label="home" className="flex">
-            <img src="/src/images/cookie.gif" alt="logo" className="w-6 h-6" />
-            <h1 className="text-lg font-bold flex font-retro">zzannorita</h1>
+          <a href="/" aria-label="home" className="flex items-center">
+            <img src="/src/images/cd.gif" alt="logo" className="w-6 h-6 mr-2" />
+            <h1 className="text-lg font-bold flex">zzannorita</h1>
           </a>
         </div>
         <div className="flex justify-between items-center">
           <img
             src="/src/images/moon.png"
             alt="light"
-            className="w-4 h-4 mx-2"
+            className="w-3 h-3 mx-2"
           />
-          <div className="text-sm font-retro text-gray-800 ">sound on</div>
+          <div className="text-xs  text-gray-800 ">sound on</div>
         </div>
       </div>
       {/* 메인헤더 */}
       <div className="sm:hidden flex justify-end items-center max-w-screen-xl mx-auto h-10">
         <nav>
-          <ul className="flex space-x-6 font-retro text-md px-2">
+          <ul className="flex space-x-6 text-sm px-2">
             <li>
               <a href="#Home" className="hover:text-yellow-500">
                 Home
@@ -54,18 +54,18 @@ export default function Header() {
       <div className="hidden sm:flex justify-end max-w-screen-xl mx-auto pt-4 ">
         <div className="flex justify-between items-center w-20">
           <img src="/src/images/moon.png" alt="light" className="w-4 h-4" />
-          <div className="font-retro text-gray-800 text-sm">sound on</div>
+          <div className=" text-gray-800 text-sm">sound on</div>
         </div>
       </div>
       {/* 메인헤더 */}
       <div className="hidden sm:flex justify-between max-w-screen-xl mx-auto pb-2 ">
         <div className="flex justify-between items-center w-full h-8">
-          <a href="/" aria-label="home" className="flex">
-            <img src="/src/images/cookie.gif" alt="logo" className="w-8 h-8 " />
-            <h1 className="text-2xl font-bold flex font-retro">zzannorita</h1>
+          <a href="/" aria-label="home" className="flex items-center">
+            <img src="/src/images/cd.gif" alt="logo" className="w-8 h-8 mr-2" />
+            <h1 className="text-xl font-bold flex ">zzannorita</h1>
           </a>
           <nav>
-            <ul className="flex space-x-6 font-retro text-xl">
+            <ul className="flex space-x-6 text-md">
               <li>
                 <a href="#Home" className="hover:text-yellow-500">
                   Home
