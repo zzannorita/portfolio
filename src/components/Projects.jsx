05219ae18b3db74544758c17fd96fbbb5a1273fc
zzannorita <tpals0518@naver.com>
2025-04-01 17:19:19 +0900

    Design: 프로젝트 페이지 프레임 구성

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function Projects() {
   return (
-    <div>
-    projects
+    <div className="w-full flex flex-col mt-28">
+      <div className="flex justify-center items-center h-[10vh] text-3xl text-black-700 font-retro">
+        Projects map
+      </div>
+      <div
+        className="w-full h-[50vh] sm:h-[70vh] lg:h-[100vh]  
+                 bg-[url('/src/images/map.png')] 
+                 bg-cover bg-center bg-no-repeat "
+      >
+        <div className="">dfdf</div>
+      </div>
     </div>
-  )
+  );
 }
