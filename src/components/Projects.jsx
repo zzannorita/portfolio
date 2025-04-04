af674f604e2b73a63174d7aaf5b69ec74e2013d4
zzannorita <tpals0518@naver.com>
2025-04-05 05:46:15 +0900

    Test: 예시 이미지 및 링크 적용

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,32 +5,51 @@ export default function Projects() {
     {
       name: "망고와 구아바",
       period: "2024.08 - 2025.01",
-      content:
-        "사용자가 간편하게 상품을 등록하고, 실시간 채팅으로 구매자와 소통할 수 있으며, 찜과 채팅 알림으로 빠른 거래가 가능한 중고거래 사이트입니다.",
-      skill: "react.js, javascript, axios, tailwindCss",
+      content: [
+        "망고와 구아바는 사용자가 간편하게 상품을 등록하고, 실시간 채팅을 통해 구매자와 원활하게 소통할 수 있는 중고거래 플랫폼입니다.  ",
+        " React.js를 기반으로 개발되었으며 Axios를 활용하여 백엔드 API와의 비동기 통신을 최적화하고, 데이터 요청 및 응답을 효율적으로 관리했습니다.",
+      ],
+
+      skill: ["react.js", "javascript", "tailwindCss", "axios"],
       gitHub: "https://github.com/zzannorita/MangoAndGuava",
-      detail: "https://www.notion.so/1c87979a0bd580e4b18bcc70b9d776a7",
+      detail: "1c87979a0bd580e4b18bcc70b9d776a7",
       position: { top: "18vh", left: "36%" },
+      img: [
+        "/src/images/project-mango-1.png",
+        "/src/images/project-mango-2.png",
+      ],
     },
     {
       name: "TeamMate",
       period: "2023.12 - 2024.02",
-      content:
-        "협업 해야하는 사람들과 팀을 만들어 문서 공유와 캘린더로 일정 공유를 할 수 있어 원활한 협업을 가능하게하는 서비스입니다.",
-      skill: "vite, react.js, typescript, axios, recoil",
+      content: [
+        "팀메이트는 협업 해야하는 사람들과 팀을 만들어 문서 공유와 캘린더로 일정 공유를 할 수 있어 원활한 협업을 가능하게하는 서비스입니다.",
+      ],
+      skill: ["vite", "react.js", "typescript", "axios", "recoil"],
       gitHub: "https://github.com/100backfro/teammate",
       notion: "https://www.notion.so/TEAMMATE-f6266a684b02452d99bcfec7a44192ed",
+      detail: "1c87979a0bd580e4b18bcc70b9d776a7",
       position: { top: "9vh", left: "54%" },
+      img: [
+        "/src/images/project-mango-1.png",
+        "/src/images/project-mango-2.png",
+      ],
     },
     {
       name: "TeamMate",
       period: "2023.12 - 2024.02",
-      content:
+      content: [
         "협업 해야하는 사람들과 팀을 만들어 문서 공유와 캘린더로 일정 공유를 할 수 있어 원활한 협업을 가능하게하는 서비스입니다.",
-      skill: "vite, react.js, typescript, axios, recoil",
+      ],
+      skill: ["vite", "react.js", "typescript", "axios", "recoil"],
       gitHub: "https://github.com/100backfro/teammate",
       notion: "https://www.notion.so/TEAMMATE-f6266a684b02452d99bcfec7a44192ed",
+      detail: "1c87979a0bd580e4b18bcc70b9d776a7",
       position: { top: "78vh", left: "28%" },
+      img: [
+        "/src/images/project-mango-1.png",
+        "/src/images/project-mango-2.png",
+      ],
     },
   ];
 
