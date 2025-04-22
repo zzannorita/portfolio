7745e0bf9e56f4c2ecd1f6672446221fff38d26a
zzannorita <tpals0518@naver.com>
2025-04-22 16:46:11 +0900

    Fix: 메인.jsx에서 css 경로 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import "./styles/tailwind.css";
+import "/index.css";
+import "/styles/tailwind.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
