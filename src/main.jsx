e72b77c1545bee88607f29dac015d960dd1d3c09
zzannorita <tpals0518@naver.com>
2025-04-22 16:50:31 +0900

    Fix: index.css는 ./추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "/index.css";
+import "./index.css";
 import "/styles/tailwind.css";
 import App from "./App.jsx";
 
